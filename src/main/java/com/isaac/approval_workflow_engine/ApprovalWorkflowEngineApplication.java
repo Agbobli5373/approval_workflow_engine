0c83da1213d024dd7a8e6bd9f575c9f5cce36341
Agbobli Isaac <agbobliisaackwadzo@gmail.com>
2026-02-21 17:33:37 +0000

    chore: initialize spring boot project scaffold

---
+++ src/main/java/com/isaac/approval_workflow_engine/ApprovalWorkflowEngineApplication.java
@@ -0,0 +1,13 @@
+package com.isaac.approval_workflow_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprovalWorkflowEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApprovalWorkflowEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isaac/approval_workflow_engine/ApprovalWorkflowEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaac.approval_workflow_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprovalWorkflowEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
